22902e2476dd1cd30ae48bcd57eb6b4bc0af3817
Leo Fridman <madfridman@gmail.com>
2022-08-11 22:55:49 +0200

    Initial Commit

---
+++ LoginEmulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginEmulator
+//
+//  Created by Leon Fridman on 8/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
