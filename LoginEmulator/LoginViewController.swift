50920c4fd843a1b20a767a50a97c6909ce01e047
Leo Fridman <madfridman@gmail.com>
2022-08-11 23:09:12 +0200

    Created Login Screen

--- LoginEmulator/LoginViewController.swift
+++ LoginEmulator/LoginViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+    }
 
 }
 
